d90f4a6ab0e1abb3a5a62520bf1446c0f12360ab
alrivasp <alvaro.rivas.p@gmail.com>
2020-05-22 18:15:24 -0400

    Correccion metodo filter

--- filtrando_hash.rb
+++ filtrando_hash.rb
@@ -1,2 +1,15 @@
-def filter arr
+ventas = {
+    Octubre: 65000,
+    Noviembre: 68000,
+    Diciembre: 72000
+    }
+
+def filter ventas_totales
+    result = {}
+    ventas_totales.each do |k, v| 
+        result[k] = v if v < 70000  
+    end
+    result
 end
+ 
+print filter(ventas).class
